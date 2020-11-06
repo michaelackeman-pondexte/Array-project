993747e9632f37d69ef0d95663c27528e02766be
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2020-11-06 18:57:12 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+class Main {
+  public static void main(String[] args) {
+
+int[ ] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+System.out.println(seats.length);
+
+String [] names = {"","","","","","","","","","","","","","",""};
+System.out.println(names.length);
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
+}
