4e082530ab0058440d010064b8eb48fcb84f4ed0
michaelackerma <Michael.Ackerman-Pondexte@patelhs.org>
2020-11-17 15:15:59 +0000

    I corrected the while loop by fixing the definition statement

--- Main.java
+++ Main.java
@@ -1,25 +1,27 @@
 import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
 
-int[ ] seats = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
-String [] names = {"","","","","","","","","","","","","","",""};
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    String[] names = new String[15];
 
-Scanner scan = new Scanner(System.in);
-for(int i = 0; i< names.length; i++){
-System.out.println(" What is your name?");
+    Scanner scan = new Scanner(System.in);
+    for (int i = 0; i < names.length; i++) {
+      System.out.println(" What is your name?");
 
-names[i] = scan.next(); 
+      names [i] = scan.next();
 
+    
+    }
+    int i = 0;// Initilization
 
-int i = 0;
-}
-while (i < 15){
- 
-System.out.println( names[0] + " sits in seat #" + seats[0]);
-i++;
+    while (i < 15) {// test
 
-}
+      System.out.println(names[i] + " sits in seat #" + seats[i]);
+      i++;// update
+  
+    }
 
   }
 }
